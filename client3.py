c6ca440623aebc96ffec2f01b5da2e18c64eddfc
sarojkumarvivek <viveksaroj579@gmail.com>
2020-08-24 11:09:33 +0000

    Create Patch File

--- client3.py
+++ client3.py
@@ -1,13 +1,3 @@
-#This is where you would manually type the existing code in the client3.py, which is inside the jpm_module_1 folder (disregard copying the useless comments) so that you can avoid the dreaded tab editing issues in REPL particularly for the Python environment only
-
-#After manually copying the useful code in client3.py, you can make your changes here
-
-#Before you test your modified code is working, make sure you have done the initial steps needed to be done before making changes in the instruction slides
-
-#Then, to test, first, delete the client3.py file inside jpm_module_1. Then rename this file to just client3.py. Finally move this file inside the jpm_module_1 folder  
-
-# Delete these comments when you're done...
-# Comments are anything that's preceded with '#'
 ################################################################################
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
@@ -49,22 +39,25 @@ def getDataPoint(quote):
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
-  if(price_b == 0):
-    return
+	""" Get ratio of price_a and price_b """
+	""" ------------- Update this function ------------- """
+	""" Also create some unit tests for this function in client_test.py """
+	if(price_b==0):
+		return
 	return price_a/price_b
 
 # Main
 if __name__ == "__main__":
 
 	# Query the price once every N seconds.
-	for _ in iter(range(N)):
+	for _ in range(N):
 		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-		
-    price = {}
+		""" ----------- Update to get the ratio --------------- """
+		prices = {}
 		for quote in quotes:
 			stock, bid_price, ask_price, price = getDataPoint(quote)
-      prices[stock] = price
+			prices[stock] = price
 			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-		print ("Ratio %s" % getRatio(price['ABC'], price['DEF']))
+		print ("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
